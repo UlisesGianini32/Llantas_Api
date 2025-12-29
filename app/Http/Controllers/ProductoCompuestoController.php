04f025646cd28cd1ba95613f7ddf8eb69bba8e05
New Llano 4k <146961271+UlisesGianini32@users.noreply.github.com>
2025-12-29 15:20:15 -0700

    producto_compuestos

--- app/Http/Controllers/ProductoCompuestoController.php
+++ app/Http/Controllers/ProductoCompuestoController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 
 class ProductoCompuestoController extends Controller
 {
+    protected $table = 'producto_compuestos';
     /* ======================================================
      | API METHODS (JSON)
      |======================================================*/
