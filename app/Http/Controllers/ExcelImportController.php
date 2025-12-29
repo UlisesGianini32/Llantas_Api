e93f44448692c6a49edfd030bc2b4e5f2a253d96
New Llano 4k <146961271+UlisesGianini32@users.noreply.github.com>
2025-12-29 16:07:12 -0700

    excelimport6

--- app/Http/Controllers/ExcelImportController.php
+++ app/Http/Controllers/ExcelImportController.php
@@ -9,15 +9,8 @@ use Maatwebsite\Excel\Facades\Excel;
 class ExcelImportController extends Controller
 {
     public function importar(Request $request)
-    {
-        $request->validate([
-            'archivo' => 'required|file|mimes:xlsx,xls',
-        ]);
+{
+    dd('SI ENTRE AL IMPORT'); // 👈 prueba dura
+}
 
-        Excel::import(new LlantasImport, $request->file('archivo'));
-
-        return redirect()
-            ->route('dashboard')
-            ->with('success', 'Excel importado correctamente');
-    }
 }
